68dffc0c522a78982743a037edc54a3a6391cb03
Felype <felyped03@gmail.com>
2026-02-12 15:57:17 -0300

    Update aspect-ratio.tsx

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,5 +1,23 @@
+import * as React from "react";
 import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+import { cn } from "@/lib/utils";
 
-const AspectRatio = AspectRatioPrimitive.Root;
+const AspectRatio = React.forwardRef<
+  React.ElementRef<typeof AspectRatioPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AspectRatioPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AspectRatioPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative overflow-hidden rounded-xl",
+      "bg-cyber-card border border-white/10",
+      "transition-all",
+      className
+    )}
+    {...props}
+  />
+));
+
+AspectRatio.displayName = "AspectRatio";
 
 export { AspectRatio };
