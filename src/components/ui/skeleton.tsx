d54eb5ce98d54c274c4e89cdaf21513f42d1076d
Felype <felyped03@gmail.com>
2026-02-12 17:31:40 -0300

    Update skeleton.tsx

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,7 +1,12 @@
+import * as React from "react";
 import { cn } from "@/lib/utils";
 
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const Skeleton: React.FC<SkeletonProps> = ({ className, ...props }) => {
   return <div className={cn("animate-pulse rounded-md bg-muted", className)} {...props} />;
-}
+};
+
+Skeleton.displayName = "Skeleton";
 
 export { Skeleton };
