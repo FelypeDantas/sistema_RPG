77f69aae645cfdfb4aa0a3e39e7d31c6fc40543c
Felype <felyped03@gmail.com>
2026-02-12 16:21:26 -0300

    Update collapsible.tsx

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,9 +1,52 @@
+import * as React from "react";
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+
+import { cn } from "@/lib/utils";
 
 const Collapsible = CollapsiblePrimitive.Root;
 
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+const CollapsibleTrigger = React.forwardRef<
+  React.ElementRef<typeof CollapsiblePrimitive.CollapsibleTrigger>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleTrigger>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.CollapsibleTrigger
+    ref={ref}
+    className={cn(
+      [
+        "flex w-full items-center justify-between",
+        "rounded-lg px-3 py-2 text-sm font-medium",
+        "transition-all duration-200",
+        "hover:bg-white/5",
+        "focus-visible:outline-none",
+        "focus-visible:ring-2 focus-visible:ring-neon-purple/40 focus-visible:ring-offset-2 focus-visible:ring-offset-cyber-dark",
+        "data-[state=open]:bg-white/5",
+      ].join(" "),
+      className
+    )}
+    {...props}
+  />
+));
+CollapsibleTrigger.displayName =
+  CollapsiblePrimitive.CollapsibleTrigger.displayName;
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+const CollapsibleContent = React.forwardRef<
+  React.ElementRef<typeof CollapsiblePrimitive.CollapsibleContent>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleContent>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.CollapsibleContent
+    ref={ref}
+    className={cn(
+      [
+        "overflow-hidden text-sm",
+        "data-[state=closed]:animate-collapsible-up",
+        "data-[state=open]:animate-collapsible-down",
+      ].join(" "),
+      className
+    )}
+    {...props}
+  />
+));
+CollapsibleContent.displayName =
+  CollapsiblePrimitive.CollapsibleContent.displayName;
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent };
